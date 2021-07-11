91769416db1250d60bfc7ed551ad93516e2f77f2
AJ-Ferguson123 <50592799+AJ-Ferguson123@users.noreply.github.com>
2021-07-11 17:54:24 -0400

    added services section and style

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+// import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
+        {/* <ScrollToTop /> */}
         <Switch>
           <Route path="/about">
             <About />
