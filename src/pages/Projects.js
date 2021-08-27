cc209d1096e65872d84f1fcfe6b2534bc5b8c05a
AJ-Ferguson123 <50592799+AJ-Ferguson123@users.noreply.github.com>
2021-08-27 09:39:34 -0400

    added picture for project added comp for addtional project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProjectItem from '..//components/ProjectItem'
-// import ProjectsSection from '..//components/ProjectsSection'
+import ProjectsSection from '..//components/ProjectsSection'
+import ProjectItemTwo from '../components/ProjectItemTwo'
 
 export default function Projects() {
     return (
         <div>
             <ProjectItem />
+            <ProjectItemTwo />
             {/* <ProjectsSection /> */}
             
         </div>
